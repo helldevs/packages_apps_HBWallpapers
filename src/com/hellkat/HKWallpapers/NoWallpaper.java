65d1dfa8ec9d473aabc265c9bd548f7fafceb5c7
thdDude <afganidude@gmail.com>
2014-03-16 00:10:38 -0500

    its late

--- src/com/hellkat/HKWallpapers/NoWallpaper.java
+++ src/com/hellkat/HKWallpapers/NoWallpaper.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.HKWallpapers;
+package com.hellkat.HKWallpapers;
 
 import android.app.Activity;
 import android.app.WallpaperManager;
